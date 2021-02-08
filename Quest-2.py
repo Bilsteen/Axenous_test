8ff5cbfa0e5deb0b2a3b4dcc44117fa46f87a447
Bilsteen <64262781+Bilsteen@users.noreply.github.com>
2021-02-08 11:19:30 +0530

    Update Quest-2.py

--- Quest-2.py
+++ Quest-2.py
@@ -1,16 +1,25 @@
 #Question 2:
-#Find number of small and capital letters in a string and replace all capital letters by same small letter and all #small letters by same capital letters
+#Find number of small and capital letters in a string and replace all capital letters by same small letter and all 
+#small letters by same capital letters
 
 
 def capital_small(word):
     new_word = []
+    cap = 0
+    small = 0
     for char in word:
         if char == " ":
             new_word.append(char)
         if char == char.upper():
+            cap += 1
             new_word.append(char.lower())
         else:
+            small += 1
             new_word.append(char.upper())
-    return "".join(new_word)
+    return cap,small,"".join(new_word)
 
-print(capital_small("EduCatiON"))
+cap,small,output = capital_small("EduCatiON")
+
+print(f"Number of capital letters are {cap}")
+print(f"Number of small letters are {small}")
+print(f"Output is {output}")
